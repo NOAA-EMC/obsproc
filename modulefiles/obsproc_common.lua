86e4abd689d5119a980acc84048b8bebd5220e2e
Rahul Mahajan <aerorahul@users.noreply.github.com>
2021-10-27 20:57:49 +0000

    partial build

--- modulefiles/obsproc_common.lua
+++ modulefiles/obsproc_common.lua
@@ -5,6 +5,7 @@ Load common modules to build obsproc on all machines
 netcdf_ver=os.getenv("netcdf_ver") or "default"
 bacio_ver=os.getenv("bacio_ver") or "default"
 w3emc_ver=os.getenv("w3emc_ver") or "default"
+w3nco_ver=os.getenv("w3nco_ver") or "default"
 sp_ver=os.getenv("sp_ver") or "default"
 sigio_ver=os.getenv("sigio_ver") or "default"
 nemsio_ver=os.getenv("nemsio_ver") or "default"
@@ -13,6 +14,7 @@ bufr_ver=os.getenv("bufr_ver") or "default"
 load(pathJoin("netcdf", netcdf_ver))
 load(pathJoin("bacio", bacio_ver))
 load(pathJoin("w3emc", w3emc_ver))
+load(pathJoin("w3nco", w3nco_ver))
 load(pathJoin("sp", sp_ver))
 load(pathJoin("sigio", sigio_ver))
 load(pathJoin("nemsio", nemsio_ver))
--- modulefiles/obsproc_wcoss2.lua
+++ modulefiles/obsproc_wcoss2.lua
@@ -4,7 +4,9 @@ Load environment to build obsproc on WCOSS2
 
 load("envvar")
 load("PrgEnv-intel")
-load(pathJoin("intel/19.1.3.304"))
+load("intel/19.1.3.304")
+load("craype/2.7.8")
+load("cray-mpich/8.1.4")
 
 -- Load common modules for this package
 load("obsproc_common")
