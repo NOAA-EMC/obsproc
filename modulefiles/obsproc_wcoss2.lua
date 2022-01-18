f29f641bb02620b8cc4bb82a6113fd4742fdd140
Shelley Melchior <Shelley.Melchior@noaa.gov>
2022-01-18 00:49:18 +0000

    Updated modulefiles/obsproc_wcoss2.lua to match what NCO SPA is using.

--- modulefiles/obsproc_wcoss2.lua
+++ modulefiles/obsproc_wcoss2.lua
@@ -2,12 +2,18 @@ help([[
 Load environment to build obsproc on WCOSS2
 ]])
 
-load("envvar")
-load("PrgEnv-intel")
-load("intel/19.1.3.304")
-load("cmake/3.20.2")
-load("craype/2.7.8")
-load("cray-mpich/8.1.4")
+PrgEnv_intel_ver=os.getenv("PrgEnv_intel_ver")
+intel_ver=os.getenv("intel_ver")
+cmake_ver=os.getenv("cmake_ver")
+craype_ver=os.getenv("craype_ver")
+cray_mpich_ver=os.getenv("cray_mpich_ver")
+
+-- load("envvar")
+load(pathJoin("PrgEnv-intel", PrgEnv_intel_ver))
+load(pathJoin("intel", intel_ver))
+load(pathJoin("cmake", cmake_ver))
+load(pathJoin("craype", craype_ver))
+load(pathJoin("cray-mpich", cray_mpich_ver))
 
 -- Load common modules for this package
 load("obsproc_common")
