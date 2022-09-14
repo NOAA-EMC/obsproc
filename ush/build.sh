4988ced645fa14da01d1262a2c7809a3e29ee457
iliana Genkova <59100254+ilianagenkova@users.noreply.github.com>
2022-09-14 15:35:03 -0500

    Reshuffled files (EIB and NCO request) (#71)
    * Reshuffled files
    * Shortened RN, updated memory in ecf global dump trigger
    * Update /ecf to NCO's /ecf

--- ush/build.sh
+++ ush/build.sh
@@ -5,8 +5,15 @@ set -eux
 # Location of PWD and package source directory.
 readonly pkg_root=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )/.." && pwd -P)
 
+# User Options
+BUILD_TYPE=${BUILD_TYPE:-"Release"}
+BUILD_DIR=${BUILD_DIR:-"$pkg_root/build"}
+CMAKE_OPTS=${CMAKE_OPTS:-}
 INSTALL_TARGET=${INSTALL_TARGET:-"wcoss2"}
 INSTALL_PREFIX=${INSTALL_PREFIX:-"$pkg_root/install"}
+
+# By default; build as if NCO is executing this script
+BUILD_4NCO=${BUILD_4NCO:-"YES"}
 
 target=$(echo $INSTALL_TARGET | tr [:upper:] [:lower:])
 if [[ "$target" =~ ^(wcoss2|hera|orion)$ ]]; then
@@ -18,15 +25,26 @@ if [[ "$target" =~ ^(wcoss2|hera|orion)$ ]]; then
   set -x
 fi
 
-# Create a build directory and cd into it.
-[[ -d build  ]] && rm -rf build
-mkdir -p build && cd build
+# Re-use or create a new BUILD_DIR (Default: create new BUILD_DIR)
+[[ ${BUILD_CLEAN:-"YES"} =~ [yYtT] ]] && rm -rf $BUILD_DIR
+mkdir -p $BUILD_DIR && cd $BUILD_DIR
+
+# Collect BUILD Options
+CMAKE_OPTS+=" -DCMAKE_BUILD_TYPE=$BUILD_TYPE"
+
+# Install destination for built executables, libraries, CMake Package config
+CMAKE_OPTS+=" -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX"
+
+# NCO/Jobs expects executables in exec/
+CMAKE_OPTS+=" -DCMAKE_INSTALL_BINDIR=exec"
 
 # build and install.
-cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX -DCMAKE_INSTALL_BINDIR=exec ..
+cmake $CMAKE_OPTS $pkg_root
 make -j ${BUILD_JOBS:-8} VERBOSE=${BUILD_VERBOSE:-}
 make install
-exit 0 # EMC can use exit here; NCO can comment this line out
+
+[[ ${BUILD_4NCO} =~ [yYtT] ]] || exit 0
+
 #############################################################################
 # This section to be removed when NCO is comfortable with the typical
 # `cmake`, `make` and `make install` process.
@@ -34,13 +52,11 @@ exit 0 # EMC can use exit here; NCO can comment this line out
 # manually copy compiled executables from `$INSTALL_PREFIX/`
 # directory to `pkg_root` and then remove `$INSTALL_PREFIX/`
 mkdir -p $pkg_root/exec
-cp -f $INSTALL_PREFIX/exec/*                                  $pkg_root/exec/
+cp -f $INSTALL_PREFIX/exec/* $pkg_root/exec/
 rm -rf $INSTALL_PREFIX
 #############################################################################
 
 # Remove build directory upon successfull build and install
-#cd $pkg_root
-cd ../
-rm -rf build
+rm -rf ${BUILD_DIR}
 
 exit 0
