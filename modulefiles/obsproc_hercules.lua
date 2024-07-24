059e3333ce26916db13f204429781905db450a6e
Kate.Friedman <Kate.Friedman@noaa.gov>
2024-07-24 18:46:40 +0000

    Update obsproc build for spack-stack and Rocky OS
    - Add build/run version files for each supported platform
    - Add version and modulefiles for new platform (Hercules)
    - Update build to use new version files
    - Update .gitignore
    
    Refs #88

--- modulefiles/obsproc_hercules.lua
+++ modulefiles/obsproc_hercules.lua
@@ -1,8 +1,8 @@
 help([[
-Load environment to build obsproc on Orion
+Load environment to build obsproc on Hercules
 ]])
 
-prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/orion/spack-stack-1.6.0/envs/gsi-addon-env-rocky9/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/hercules/spack-stack-1.6.0/envs/gsi-addon-env/install/modulefiles/Core")
 
 stack_intel_ver=os.getenv("stack_intel_ver") or "2021.9.0"
 stack_impi_ver=os.getenv("stack_impi_ver") or "2021.9.0"
@@ -10,9 +10,13 @@ cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 
 load(pathJoin("stack-intel", stack_intel_ver))
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
+
 load(pathJoin("cmake", cmake_ver))
 
 -- Load common modules for this package
 load("obsproc_common")
+
+-- On Hercules, MKL needs to be loaded separately
+load("intel-oneapi-mkl/2022.2.1")
 
 whatis("Description: obsproc build environment")
--- ush/build.sh
+++ ush/build.sh
@@ -7,56 +7,49 @@ readonly pkg_root=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )/.."
 
 # User Options
 BUILD_TYPE=${BUILD_TYPE:-"Release"}
-BUILD_DIR=${BUILD_DIR:-"$pkg_root/build"}
+BUILD_DIR=${BUILD_DIR:-"${pkg_root}/build"}
 CMAKE_OPTS=${CMAKE_OPTS:-}
 INSTALL_TARGET=${INSTALL_TARGET:-"wcoss2"}
-INSTALL_PREFIX=${INSTALL_PREFIX:-"$pkg_root/install"}
+INSTALL_PREFIX=${INSTALL_PREFIX:-"${pkg_root}/install"}
 
 # By default; build as if NCO is executing this script
 BUILD_4NCO=${BUILD_4NCO:-"YES"}
 
-target=$(echo $INSTALL_TARGET | tr [:upper:] [:lower:])
-if [[ "$target" =~ ^(wcoss2|hera|orion|jet)$ ]]; then
-  source $pkg_root/versions/build.ver
+target="${INSTALL_TARGET,,}"
+if [[ "${target}" =~ ^(wcoss2|hera|orion|jet|hercules)$ ]]; then
+  # prepare the target specific build.ver and run.ver
+  cd "${pkg_root}/versions" || exit 1
+  rm -f build.ver run.ver
+  cp "build.${target}.ver" "build.ver"
+  cp "run.${target}.ver"   "run.ver"
+  cd "${pkg_root}" || exit 1
+  export HOMEobsproc="${pkg_root}"
+  source "${pkg_root}/versions/build.ver"
+  unset HOMEobsproc
   set +x
-  module use $pkg_root/modulefiles
-  module load obsproc_$target
+  module purge
+  module use "${pkg_root}/modulefiles"
+  module load "obsproc_${target}"
   module list
   set -x
+else
+  echo "WARNING: Unknown target"
 fi
 
-# Re-use or create a new BUILD_DIR (Default: create new BUILD_DIR)
-[[ ${BUILD_CLEAN:-"YES"} =~ [yYtT] ]] && rm -rf $BUILD_DIR
-mkdir -p $BUILD_DIR && cd $BUILD_DIR
-
-# Collect BUILD Options
-CMAKE_OPTS+=" -DCMAKE_BUILD_TYPE=$BUILD_TYPE"
-
-# Install destination for built executables, libraries, CMake Package config
-CMAKE_OPTS+=" -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX"
-
-# NCO/Jobs expects executables in exec/
-CMAKE_OPTS+=" -DCMAKE_INSTALL_BINDIR=exec"
+# Create a build directory and cd into it.
+[[ -d build  ]] && rm -rf build
+mkdir -p build && cd build
 
 # build and install.
-cmake $CMAKE_OPTS $pkg_root
-make -j ${BUILD_JOBS:-8} VERBOSE=${BUILD_VERBOSE:-}
+cmake -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" \
+      -DCMAKE_INSTALL_BINDIR=exec \
+      -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" \
+      ..
+make -j "${BUILD_JOBS:-6}" VERBOSE="${BUILD_VERBOSE:-}"
 make install
 
-[[ ${BUILD_4NCO} =~ [yYtT] ]] || exit 0
-
-#############################################################################
-# This section to be removed when NCO is comfortable with the typical
-# `cmake`, `make` and `make install` process.
-# To abide by current NCO working practices,
-# manually copy compiled executables from `$INSTALL_PREFIX/`
-# directory to `pkg_root` and then remove `$INSTALL_PREFIX/`
-mkdir -p $pkg_root/exec
-cp -f $INSTALL_PREFIX/exec/* $pkg_root/exec/
-rm -rf $INSTALL_PREFIX
-#############################################################################
-
 # Remove build directory upon successfull build and install
-rm -rf ${BUILD_DIR}
+cd "${pkg_root}"
+rm -rf build
 
 exit 0
