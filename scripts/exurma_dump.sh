4b5794f519ea83dda4b60f8c9eef27771c68536e
Iliana Genkova <iliana.genkova@noaa.gov>
2025-01-23 15:17:44 +0000

    Increase TANK_MAX_255004 to process larger tanks (due to decod_dcmeso_v4.1.3 update)

--- scripts/exurma_dump.sh
+++ scripts/exurma_dump.sh
@@ -73,7 +73,7 @@ err7=0
 #restrict processing of unexpected big tanks
 #this block appear in all /scripts/ex*_dump.sh proessing msone0 and msone1 
 TANK_MAX_255003=${TANK_MAX_255003:-3221225472} #3Gb
-TANK_MAX_255004=${TANK_MAX_255004:-1610612736} #1.5Gb
+TANK_MAX_255004=${TANK_MAX_255004:-2684354560} #2.5Gb
 TANK_MAX_255030=${TANK_MAX_255030:-4187593114} #3.9Gb
 if [ -s ${TANK}/${PDY}/b255/xx003 ]&& [ "$(stat -c '%s' ${TANK}/${PDY}/b255/xx003)" -gt "$TANK_MAX_255003" ]; then
  export SKIP_255003=YES
